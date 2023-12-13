e0c5228b6060a93d406ce61ec4d3ee6643d30725
Propitive <propitive@gmail.com>
2023-12-12 21:40:34 -0600

    styled item modal delete button

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -31,13 +31,32 @@ function ItemModal({ card, onClose, onOpenDeleteModal }) {
           <div className="item-modal__description">
             <p className="item-modal__weather">Weather: {card.weather}</p>
             {isCardOwner && (
-              <button
-                className="item-modal__delete"
-                onClick={onOpenDeleteModal}
-                type="button"
-              >
-                Delete item
-              </button>
+              <>
+                {/* <button
+                  className="item-modal__delete"
+                  onClick={onOpenDeleteModal}
+                  type="button"
+                >
+                  Delete item
+                </button> */}
+                <button
+                  className="item-modal__delete-buton"
+                  onClick={onOpenDeleteModal}
+                  type="button"
+                >
+                  <span className="item-modal__delete-text">Delete</span>
+                  <span className="icon">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                    </svg>
+                  </span>
+                </button>
+              </>
             )}
           </div>
         </div>
